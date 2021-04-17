374a2c5b1709eb78de890b5a52a3aa9fda8a50ad
siwony_ <siwon103305@gmail.com>
2021-04-18 04:57:20 +0900

    [ADD]basicMapping

---
+++ src/main/java/basicMapping_02/entity/Member.java
@@ -0,0 +1,28 @@
+package basicMapping_02.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    List<Order> order = new ArrayList<>();
+
+
+}
